d243b5e0c532633c2db6fe2ccc01188f7025f819
Tumi Mashigo <tumi.mashigo15@gmail.com>
2023-05-11 15:02:34 +0200

    added more features

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Lotto from './Lotto';
 import LottoHowTo from './lottoHowTo';
 import DailyLotto from './dailyLotto';
 import PowerBall from './Powerball';
+import PowerballHowTo from './PowerballHowTo';
+import Sportstake8 from './Sportstake8';
+import Propick from './Propick';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -14,6 +17,10 @@ function App() {
           <Route path="/lottoHowTo" element={<LottoHowTo />} />
           <Route path="/dailyLotto" element={<DailyLotto />} />
           <Route path="/Powerball" element={<PowerBall />} />
+          <Route path="/PowerballHowTo" element={<PowerballHowTo />} />
+          <Route path="/Sportstake8" element={<Sportstake8 />} />
+          <Route path="/Propick" element={<Propick />} />
+          Propick
         </Routes>
       </div>
     </Router>
