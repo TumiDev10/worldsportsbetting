7a30815bca77e4bd3e7d1dd803aed3b91d000f0d
Tumi Mashigo <tumi.mashigo15@gmail.com>
2023-05-11 10:34:18 +0200

    added new feautres

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Lotto from './Lotto';
 import LottoHowTo from './lottoHowTo';
-import DailyLotto from './dailyLotto'
+import DailyLotto from './dailyLotto';
+import PowerBall from './Powerball';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Lotto />} />
           <Route path="/lottoHowTo" element={<LottoHowTo />} />
           <Route path="/dailyLotto" element={<DailyLotto />} />
+          <Route path="/Powerball" element={<PowerBall />} />
         </Routes>
       </div>
     </Router>
